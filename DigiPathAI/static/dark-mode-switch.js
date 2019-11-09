09df45a03aa9898b5f9b931f9133aee96d421ce1
haranrk <haranrajkumar97@gmail.com>
2019-11-09 12:20:44 +0530

    Fix dark mode issues

--- DigiPathAI/static/dark-mode-switch.js
+++ DigiPathAI/static/dark-mode-switch.js
@@ -1,3 +1,15 @@
+function dark_on(){
+    $('nav').removeClass("navbar-light");
+    $('nav').addClass("navbar-dark");
+    $('#get-segmentation-btn').removeClass('btn-success');
+    $('#get-segmentation-btn').addClass('btn-dark');
+}
+function dark_off(){
+    $('nav').removeClass("navbar-dark");
+    $('nav').addClass("navbar-light");
+    $('#get-segmentation-btn').removeClass('btn-dark');
+    $('#get-segmentation-btn').addClass('btn-success');
+}
 (function() {
   var darkSwitch = document.getElementById("darkSwitch");
   if (darkSwitch) {
@@ -10,16 +22,23 @@
         localStorage.getItem("darkSwitch") !== null &&
         localStorage.getItem("darkSwitch") === "dark";
       darkSwitch.checked = darkThemeSelected;
-      darkThemeSelected
-        ? document.body.setAttribute("data-theme", "dark")
-        : document.body.removeAttribute("data-theme");
+      if(darkThemeSelected){
+            document.body.setAttribute("data-theme", "dark")
+            dark_on();
+        }
+        else{
+            document.body.removeAttribute("data-theme");
+            dark_off();
+        }
     }
     function resetTheme() {
       if (darkSwitch.checked) {
         document.body.setAttribute("data-theme", "dark");
+        dark_on();
         localStorage.setItem("darkSwitch", "dark");
       } else {
         document.body.removeAttribute("data-theme");
+        dark_off();
         localStorage.removeItem("darkSwitch");
       }
     }
